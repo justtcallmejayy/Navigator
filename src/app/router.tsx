dd3e22c84c1410faf7cb054da50ed28e77c906c2
JAY CHOKSI <150403644+justtcallmejayy@users.noreply.github.com>
2026-03-30 17:10:05 -0400

    Remove unused routes from router
    Removed unused routes for Interactive Learning, Flashcards, Quiz, and Analysis pages.

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,12 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { AppLayout } from '../components/layout';
 import AdminDashboard from '../pages/AdminDashboard';
 import AdminLogin from '../pages/AdminLogin';
-import FlashcardsPage from '../pages/FlashcardsPage';
-import AnalysisToolkit from '../pages/AnalysisToolkit';
-import AnalysisWorkspace from '../pages/AnalysisWorkspace';
 import Home from '../pages/Home';
-import InteractiveLearning from '../pages/InteractiveLearning';
-import QuizPage from '../pages/QuizPage';
 import TheoryDetail from '../pages/TheoryDetail';
 import TheoryLibrary from '../pages/TheoryLibrary';
 import VocabularyHub from '../pages/VocabularyHub';
@@ -19,12 +14,7 @@ export const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/theory', element: <TheoryLibrary /> },
       { path: '/theory/:slug', element: <TheoryDetail /> },
-      { path: '/learn', element: <InteractiveLearning /> },
-      { path: '/flashcards', element: <FlashcardsPage /> },
       { path: '/vocabulary', element: <VocabularyHub /> },
-      { path: '/quiz/:theoryId', element: <QuizPage /> },
-      { path: '/analysis', element: <AnalysisToolkit /> },
-      { path: '/analysis/:filmId', element: <AnalysisWorkspace /> },
       { path: '/admin/login', element: <AdminLogin /> },
       { path: '/admin', element: <AdminDashboard /> },
     ],
